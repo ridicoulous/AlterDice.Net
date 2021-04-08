9de2563d2b2f1089064dc7b0c421ff0305e01766
ridicoulous <artemkuryanov@gmail.com>
2021-04-08 19:49:47 +0300

    new instead override props

--- AlterDice.Net/Objects/AlterDiceActiveOrder.cs
+++ AlterDice.Net/Objects/AlterDiceActiveOrder.cs
@@ -5,17 +5,17 @@ namespace AlterDice.Net.Objects
     public class AlterDiceActiveOrder: AlterDiceOrder
     {
         [JsonProperty("volume")]
-        public override decimal Quantity { get; set; }
+        public new decimal Quantity { get; set; }
         [JsonProperty("volume_done")]
-        public override decimal QuantityDone { get; set; }
+        public new decimal QuantityDone { get; set; }
 
         [JsonProperty("price")]
-        public override decimal QuoteQuantity { get; set; }
+        public new decimal QuoteQuantity { get; set; }
         [JsonProperty("price_done")]
-        public override decimal? QuoteQuantityFilled { get; set; }
+        public new decimal? QuoteQuantityFilled { get; set; }
 
         [JsonProperty("rate")]
-        public override decimal Price { get; set; }
+        public new decimal Price { get; set; }
 
     }
 }
